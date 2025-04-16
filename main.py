5d46630291a038092629befadca0a8ea90db06f7
EstherHuang <w41621@yahoo.com.tw>
2025-04-16 11:12:28 +0800

    [test] v6

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 import os
 import uvicorn
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 app = FastAPI()
 
