51bbacecb8cbf2bf70a519d85b4d1a1da571befe
EstherHuang <w41621@yahoo.com.tw>
2025-11-10 17:48:19 +0800

    更新資料庫

--- database.py
+++ database.py
@@ -1,25 +1,29 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 url = URL.create(
     drivername="mysql+mysqlconnector",
     username="User",
-    password="User$1234",  # 這裡可直接放原文，不需手動編碼
+    password="User%1234",  
     host="tpe1.clusters.zeabur.com",
     port=21004,
-    database="dreamlinker",
+    database="my_chat_app",  
     query={"charset": "utf8mb4"},
 )
 
+# 建立連線字串
 engine = create_engine(url, echo=True, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# 🚀 建立資料表
 def init_db():
-    from models import Base
+    from models import Base  
     Base.metadata.create_all(bind=engine)
 
+# 取得 DB Session
 def get_db():
     db = SessionLocal()
     try:
